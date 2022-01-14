4cb0c906b25893b65330278a473b3601d52a4509
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-01-14 17:06:14 +0100

    [WASM] Initialize constant fields by wasm-initializers

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/FieldInitializersLowering.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/FieldInitializersLowering.kt
@@ -14,6 +14,8 @@ import org.jetbrains.kotlin.ir.builders.irSetField
 import org.jetbrains.kotlin.ir.declarations.IrField
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.expressions.IrBlockBody
+import org.jetbrains.kotlin.ir.expressions.IrConst
+import org.jetbrains.kotlin.ir.expressions.IrConstKind
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
@@ -45,6 +47,8 @@ class FieldInitializersLowering(val context: WasmBackendContext) : FileLoweringP
 
                 if (!declaration.isStatic) return
                 val initValue: IrExpression = declaration.initializer?.expression ?: return
+
+                if (initValue is IrConst<*> && initValue.kind !is IrConstKind.String && initValue.kind !is IrConstKind.Null) return
 
                 startFunctionBody.statements.add(
                     builder.at(initValue).irSetField(null, declaration, initValue)
